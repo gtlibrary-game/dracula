916a2b6c2682e324643b16b6e0c4ccead27e8dbf
HoneyLee <HoneyLee200046@gmail.com>
2023-06-15 07:54:48 +0800

    update: character Selection Dropdown

--- Assets/Script/UIHeroNFTsTab.cs
+++ Assets/Script/UIHeroNFTsTab.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using TMPro;
 public class UIHeroNFTsTab : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject nftManager;
+    public TMP_Dropdown characterDropDown;
+    public NetworkManagerMMO manager; 
+    public List<string> list = new List<string>() {"test0 (to)", "test1 (t1)" };
     void Start()
     {
         
@@ -13,6 +18,16 @@ public class UIHeroNFTsTab : MonoBehaviour
     void OnEnable(){
 
         nftManager.GetComponent<NFTManager>().getBookmarkByWallet();
+
+        handleCharacterDropdown();
+    }
+
+    public void handleCharacterDropdown(){
+
+        characterDropDown.options.Clear();
+        characterDropDown.options = manager.playerClasses.Select(
+                                    p => new TMP_Dropdown.OptionData(p.name)
+                                ).ToList();
     }
     // Update is called once per frame
     void Update()
